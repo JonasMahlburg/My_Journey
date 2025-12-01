1cf21e13d4d1d0e9c25531feeee35887cee98e49
JonasMahlburg <darrylmanga@yahoo.com>
2025-12-01 07:57:41 +0100

    Initial Commit

---
+++ My_Journy/My_JournyApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_JournyApp.swift
+//  My_Journy
+//
+//  Created by Jonas Mahlburg on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct My_JournyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
